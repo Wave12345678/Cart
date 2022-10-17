783bdfcfc44a69c4047c5a90910d06cc57518c92
Ibrahimov Bekzod <ibrahimov0411@gmail.com>
2022-10-17 11:31:53 +0500

    Cart

---
+++ js/calcCartPrice.js
@@ -0,0 +1,23 @@
+function calcCartPrice() {
+    const cartItems = document.querySelectorAll('.cart-item')
+
+    const totalPriceEl = document.querySelector('.total-price')
+
+    let totalPrice = 0;
+    cartItems.forEach(function (item) {
+        const amountEl = item.querySelector('[data-counter]')
+        const priceEl = item.querySelector('.price__currency')
+        const currentPrice = parseInt(amountEl.innerText) * parseInt(priceEl.innerText)
+        totalPrice += currentPrice
+
+
+
+    })
+
+    totalPriceEl.innerText = totalPrice
+
+
+    if (totalPrice >= 600) {
+
+    }
+}
---
+++ js/toggleCartStatus.js
@@ -0,0 +1,17 @@
+function toggleCartStatus() {
+
+    const cartWrapper = document.querySelector('.cart-wrapper')
+    const cartEmpty = document.querySelector('[data-cart-empty]')
+    const orderForm = document.querySelector('#order-form')
+
+    if (cartWrapper.children.length > 0) {
+        console.log("full");
+        cartEmpty.classList.add('none')
+        orderForm.classList.remove('none')
+    } else {
+        console.log('empty');
+        cartEmpty.classList.remove('none')
+        orderForm.classList.add('none')
+
+    }
+}
